990680c0a00851f76e652c87130bd1e1b6782f8f
Ana Rebelo <anagmrebelo@hotmail.com>
2023-09-14 13:06:01 +0200

    baseURL temporarily as production only

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 import Footer from "./Footer";
 import MainView from "./MainView";
 
-export const baseURL =
-    process.env.NODE_ENV === "production"
-        ? "https://c7c1-study-resource-catalog.onrender.com"
-        : "http://localhost:4000";
+export const baseURL = "https://c7c1-study-resource-catalog.onrender.com";
+// process.env.NODE_ENV === "production"
+//     ? "https://c7c1-study-resource-catalog.onrender.com"
+//     : "http://localhost:4000";
 
 function App() {
     return (
