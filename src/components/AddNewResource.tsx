ca74223a7abfccec8df784139823100828f2768a
Julieta-Sanguedolce <julieta.sanguedolce@gmail.com>
2023-09-19 15:23:39 +0100

    added alert functionality on the modal and got the check url post request to work

--- src/components/AddNewResource.tsx
+++ src/components/AddNewResource.tsx
@@ -9,6 +9,10 @@ import {
     ModalBody,
     ModalFooter,
     Input,
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { baseURL } from "./App";
@@ -17,16 +21,30 @@ import { useState } from "react";
 export default function AddNewResource(): JSX.Element {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [userUrl, setUserUrl] = useState("");
+    const [urlStatus, setUrlStatus] = useState<number>();
+
+    function handleCancel() {
+        onClose();
+        setUrlStatus(0);
+    }
 
     async function handleCheckURL() {
-        try {
-            const response = await axios.post(`${baseURL}/recommendation/url`, {
-                url: userUrl,
-            });
-            const responseInfo = response.data;
-            console.log(responseInfo);
-        } catch (error) {
-            console.error("this is our error", error);
+        if (userUrl === "") {
+            alert("Please input a valid URL");
+        } else {
+            try {
+                const response = await axios.post(
+                    `${baseURL}/recommendation/url`,
+                    {
+                        url: userUrl,
+                    }
+                );
+                const responseInfo = response.status;
+                setUrlStatus(responseInfo);
+                console.log(responseInfo);
+            } catch (error) {
+                console.error("this is our error", error);
+            }
         }
     }
 
@@ -51,13 +69,38 @@ export default function AddNewResource(): JSX.Element {
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                        <Button
+                            colorScheme="blue"
+                            mr={3}
+                            onClick={handleCancel}
+                        >
                             Cancel
                         </Button>
-                        <Button variant="ghost" onClick={handleCheckURL}>
+                        <Button variant="ghost" mr={3} onClick={handleCheckURL}>
                             Check URL
                         </Button>
+                        {urlStatus === 201 && (
+                            <Button colorScheme="green">Proceed</Button>
+                        )}
                     </ModalFooter>
+                    {urlStatus === 202 && (
+                        <Alert status="error">
+                            <AlertIcon />
+                            <AlertTitle>URL Already exists!</AlertTitle>
+                            <AlertDescription>
+                                Please try inputting a different URL or cancel
+                            </AlertDescription>
+                        </Alert>
+                    )}
+                    {urlStatus === 201 && (
+                        <Alert status="success">
+                            <AlertIcon />
+                            <AlertTitle>Valid URL</AlertTitle>
+                            <AlertDescription>
+                                Please add the new resource
+                            </AlertDescription>
+                        </Alert>
+                    )}
                 </ModalContent>
             </Modal>
         </>
