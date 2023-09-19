ac79cfcc374d5cd4470879b35c19103e8d494b81
Ana Rebelo <anagmrebelo@hotmail.com>
2023-09-19 17:30:23 +0200

    Send vote requests WIP

--- src/components/VotingArea.tsx
+++ src/components/VotingArea.tsx
@@ -3,6 +3,8 @@ import { FiThumbsUp, FiThumbsDown } from "react-icons/fi";
 import { Recommendation } from "./RecommendationBoard";
 import { useState } from "react";
 import { User } from "../types/User";
+import axios from "axios";
+import { baseURL } from "./App";
 
 interface VotingAreaProps {
     currentUser: User | undefined;
@@ -16,11 +18,32 @@ export default function VotingArea({
     const [voteState, setVoteState] = useState<boolean | undefined>(undefined);
 
     // { user_id, url, is_like }
-    const handleVote = (value: boolean) => {
+    const handleVote = async (value: boolean) => {
         const newVote = value === voteState ? undefined : value;
-        // true => post request /votes {is_like: value, url: oneRecommendation.url, user_id: }
         setVoteState(newVote);
+
+        // // WIP
+        // if (newVote === undefined) {
+        //     try {
+        //         await axios.delete(baseURL + "/votes", {
+        //             data: {
+        //                 url: oneRecommendation.url,
+        //                 user_id: currentUser?.id,
+        //             },
+        //         });
+        //     } catch (error) {
+        //         console.error(
+        //             "Handle error from the handle vote function:",
+        //             error
+        //         );
+        //     }
+        // }
+        // true => post request /votes {is_like: true, url: oneRecommendation.url, user_id: current_user.user_id }
+        // false => post request /votes {is_like: false, url: oneRecommendation.url, user_id: current_user.user_id }
+        // undefined => post request /votes {url: oneRecommendation.url, user_id: current_user.user_id }
     };
+
+    //   delete /votes/id/www.google.com/3
 
     const setVoteBorder = (value: boolean) => {
         return value === voteState ? "2px solid black" : "";
